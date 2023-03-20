06c6d8c00bf677d0087060128a3e2faaeea37010
5hyun <qhslsl@gmail.com>
2023-03-20 21:51:50 +0900

    Feat: clearMock true로 설정

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   // cacheDirectory: "/private/var/folders/nv/0wjrk8wd7bv9pdrb_23tlf640000gn/T/jest_dx",
 
   // Automatically clear mock calls, instances, contexts and results before every test
-  // clearMocks: false,
+  clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: false,
